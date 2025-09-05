0b62877a4cad3fdb18d5f88faf9607aa4c02ca08
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2025-09-04 23:40:20 -0500

    Add Contentful

---
+++ src/lib/contentful.ts
@@ -0,0 +1,24 @@
+import * as contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export interface ContentfulEvent {
+  contentTypeId: "event";
+  fields: {
+    title: EntryFieldTypes.Text;
+    startDate: EntryFieldTypes.Date;
+    endDate?: EntryFieldTypes.Date;
+    address: EntryFieldTypes.Text;
+    locationLink: EntryFieldTypes.Text;
+    description?: EntryFieldTypes.Text;
+    ticketsLink?: EntryFieldTypes.Text;
+    coverPhoto?: EntryFieldTypes.AssetLink;
+  };
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
